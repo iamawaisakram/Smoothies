5e23d0256d8c97be4feb2e723ed8c2bb76f6fda8
Awais Akram Mughal <iamawaisakram@gmail.com>
2020-02-01 13:31:29 +0500

    Show smoothies cards on index file

---
+++ src/components/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="index container">
+    <div class="card" v-for="(smoothie, index) in smoothies" :key="index">
+      <div class="card-content">
+        <h2 class="indigo-text">{{ smoothie.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      smoothies: [
+        {
+          title: "Morning Dew",
+          slug: "morning-dew",
+          ingredients: ["lime", "peanut butter", "mango"]
+        },
+        {
+          title: "Evening fountain",
+          slug: "evening-fountain",
+          ingredients: ["banana", "lemon", "kiwi"]
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.index {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
+}
+
+.index h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+
+.index .ingredients {
+  margin: 30px auto;
+}
+
+.index .ingredients li {
+  display: inline-block;
+}
+</style>
